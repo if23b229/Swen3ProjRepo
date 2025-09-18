69f7959e9bc9fc5134c58cc0f1021b3b65288fdd
KiriN <if23b229@technikum-wien.at>
2025-09-18 03:03:25 +0200

    Commit: REPO Main Branche, SpringBoot, REST API

---
+++ src/main/java/com/swen3/paperless/PaperlessApp.java
@@ -0,0 +1,13 @@
+package com.swen3.paperless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication
+public class PaperlessApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PaperlessApp.class, args);
+        }
+    }

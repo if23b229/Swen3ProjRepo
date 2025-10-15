93b3bc221579d26915ba2a26c5639ed61ecc54dd
Edin Licina <edin.licina.2@gmail.com>
2025-10-15 23:29:21 +0200

    added error handling

--- src/main/java/com/swen3/paperless/controller/DocumentController.java
+++ src/main/java/com/swen3/paperless/controller/DocumentController.java
@@ -1,13 +1,12 @@
 package com.swen3.paperless.controller;
 
 
-import com.swen3.paperless.dto.DocumentUploadDto;
+import com.swen3.paperless.exception.BadRequestException;
+import com.swen3.paperless.exception.TransferFailedException;
 import com.swen3.paperless.service.DocumentService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
@@ -21,9 +20,13 @@ public class DocumentController {
         this.documentService = documentService;
     }
 
-    @PostMapping(consumes = "application/json", produces = "application/json")
-    public ResponseEntity<?> enqueue(@RequestBody DocumentUploadDto dto) {
-        documentService.uploadDocument(dto);
+    @PostMapping(value = "/upload", consumes = {"multipart/form-data"})
+    public ResponseEntity<?> uploadDocument(@RequestPart("file")MultipartFile file) {
+        try{
+            documentService.uploadDocument(file);
+        } catch (BadRequestException | TransferFailedException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/swen3/paperless/service/DocumentService.java
+++ src/main/java/com/swen3/paperless/service/DocumentService.java
@@ -1,9 +1,13 @@
 package com.swen3.paperless.service;
 
-import com.swen3.paperless.dto.DocumentUploadDto;
+import com.swen3.paperless.exception.BadRequestException;
+import com.swen3.paperless.exception.TransferFailedException;
 import com.swen3.paperless.model.OcrRequestEvent;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Instant;
 import java.util.UUID;
 
@@ -15,26 +19,36 @@ public class DocumentService {
         this.queuePublisherService = queuePublisherService;
     }
 
-    public void uploadDocument(DocumentUploadDto dto){
-        String corrId = getCorrelationId(dto);
+    public void uploadDocument(MultipartFile file) throws BadRequestException, TransferFailedException {
+        String originalFilename = file.getOriginalFilename();
+        if (originalFilename == null) {
+            throw new BadRequestException("Invalid Filename");
+        }
+        if (!originalFilename.endsWith(".txt")) {
+            throw new BadRequestException("Only .txt files are allowed");
+        }
 
-        var evt = new OcrRequestEvent(
-                dto.documentId(),
-                dto.originalFilename(),
-                dto.storageUri(),
+        File savedFile = new File("uploads/" + file.getOriginalFilename());
+        savedFile.getParentFile().mkdirs();
+
+        try {
+            file.transferTo(savedFile);
+        } catch (IOException e) {
+            throw new TransferFailedException("File could not be transferred");
+        }
+
+        String documentId = UUID.randomUUID().toString();
+        String storageUri = savedFile.getAbsolutePath();
+        String correlationId = UUID.randomUUID().toString();
+
+        var event = new OcrRequestEvent(
+                documentId,
+                file.getOriginalFilename(),
+                storageUri,
                 Instant.now(),
-                corrId
+                correlationId
         );
 
-        queuePublisherService.sendToOcr(evt);
-    }
-
-    private String getCorrelationId(DocumentUploadDto dto) {
-        String correlationId = dto.correlationId();
-        if (correlationId != null && !correlationId.isBlank()) {
-            return correlationId;
-        } else {
-            return UUID.randomUUID().toString();
-        }
+        queuePublisherService.sendToOcr(event);
     }
 }
